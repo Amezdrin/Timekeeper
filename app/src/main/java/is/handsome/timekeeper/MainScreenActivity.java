b751e1fd9209a561cdc40720b467b1d4b4d84a53
Artem Mezdrin <artemmezdrin@gmail.com>
2016-04-27 14:31:00 +0600

    Added pushed state for Skip button

--- app/src/main/java/is/handsome/timekeeper/MainScreenActivity.java
+++ app/src/main/java/is/handsome/timekeeper/MainScreenActivity.java
@@ -1,42 +1,62 @@
 package is.handsome.timekeeper;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class MainScreenActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_screen_activity);
-    }
+public class MainScreenActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
-    public void dropDownListHandler(){
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main_screen_activity);
 
-        Spinner dropDown = (Spinner) findViewById(R.id.projectsDropDown);
+            // Spinner element
+            Spinner spinner = (Spinner) findViewById(R.id.projectsDropDown);
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.projects, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        dropDown.setAdapter(adapter);
+            // Spinner click listener
+            spinner.setOnItemSelectedListener(this);
 
-        dropDown.setOnItemSelectedListener(this);
-    }
+            // Spinner Drop down elements
+            List<String> categories = new ArrayList<String>();
+            categories.add("Add new project");
+            categories.add("bthere");
+            categories.add("Outingz");
+            categories.add("Pixel Perfect");
+            categories.add("Ercot");
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        parent.getItemAtPosition(1);
-    }
+            // Creating adapter for spinner
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
+            // Drop down layout style - list view with radio button
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-    }
+            // attaching data adapter to spinner
+            spinner.setAdapter(dataAdapter);
+        }
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            // On selecting a spinner item
+            String item = parent.getItemAtPosition(position).toString();
+
+            // Showing selected spinner item
+            Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+        }
+
+
 }
+
--- app/src/main/java/is/handsome/timekeeper/Onboarding.java
+++ app/src/main/java/is/handsome/timekeeper/Onboarding.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Onboarding extends AppCompatActivity {
 
@@ -19,16 +20,13 @@ public class Onboarding extends AppCompatActivity {
         View tipOne = findViewById(R.id.first_tip_layout);
         View tipTwo = findViewById(R.id.second_tip_layout);
         View tipThree = findViewById(R.id.third_tip_layout);
-        View nextBtn = findViewById(R.id.changeTipBtn);
-        View doneBtn = findViewById(R.id.gotItBtn);
+        Button nextBtn = (Button) findViewById(R.id.changeTipBtn);
 
         if (tipOne.getVisibility() == View.GONE && tipTwo.getVisibility() == View.VISIBLE) {
 
             tipTwo.setVisibility(View.GONE);
             tipThree.setVisibility(View.VISIBLE);
-            nextBtn.setVisibility(View.GONE);
-            doneBtn.setVisibility(View.VISIBLE);
-
+            nextBtn.setText("Got It!");
         }
         else {
 
@@ -36,8 +34,6 @@ public class Onboarding extends AppCompatActivity {
 
                 tipOne.setVisibility(View.GONE);
                 tipTwo.setVisibility(View.VISIBLE);
-                nextBtn.setVisibility(View.VISIBLE);
-                doneBtn.setVisibility(View.INVISIBLE);
             }
         }
     }
